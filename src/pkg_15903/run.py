98dba4127a2258647e07bfafac7de3be1d7e2721
Andre Choi <andreclc@gmail.com>
2025-05-05 19:41:41 -0700

    update-cli-typo

--- src/pkg_15903/run.py
+++ src/pkg_15903/run.py
@@ -11,7 +11,7 @@ from pkg_15903 import __version__
 
 
 @click.command()
-@click.option('--url', required=False, default='https://google.com', help='enter URL')
+@click.option('--url', required=False, default='https://google.com', help='Enter URL')
 @click.version_option(version=__version__)
 def main(url):
     response = requests.get(url)
