da63e2ce1312d5fb4580c10ba1a180645912bcc4
Andre Choi <andreclc@gmail.com>
2026-04-16 22:10:18 -0700

    style: fix docstring 2

--- tests/test_run.py
+++ tests/test_run.py
@@ -27,32 +27,32 @@ class TestMain(unittest.TestCase):
         self.runner = CliRunner()
         self.client = TestClient(app)
 
-    """ assert zero exit code with url success """
     def test_cli_main_success(self):
+        """Assert zero exit code with url success"""
         result = self.runner.invoke(main, ['--url', 'https://example.com'])
         self.assertEqual(result.exit_code, 0)
 
-    """ assert non-zero exit code with ClsValidate chk_url raise ValueError """
     def test_cli_main_validate_error(self):
+        """Assert non-zero exit code with ClsValidate chk_url raise ValueError"""
         result = self.runner.invoke(main, ['--url', 'https://iamnotarobotdot.ai'])
         self.assertNotEqual(result.exit_code, 0)
 
-    """ assert non-zero exit code with HTTPError """
     def test_cli_main_http_error(self):
+        """Assert non-zero exit code with HTTPError"""
         result = self.runner.invoke(main, ['--url', 'https://example.com/thepathdoesnotexist'])
         self.assertNotEqual(result.exit_code, 0)
 
-    """ assert ConnectionError in mock_get result.exception """
     @patch('requests.get')
     def test_cli_main_connection_error(self, mock_get):
+        """Assert ConnectionError in mock_get result.exception"""
         mock_get.side_effect = requests.exceptions.ConnectionError()
         result = self.runner.invoke(main, ['--url', 'https://example.com'])
         # result.exception: 500: Error_Type - ConnectionError
         self.assertIn("500: Error_Type - ConnectionError", str(result.exception))
 
-    """ assert RequestException in mock_get result.exception """
     @patch('requests.get')
     def test_cli_main_requests_error(self, mock_get):
+        """Assert RequestException in mock_get result.exception"""
         mock_get.side_effect = requests.exceptions.RequestException()
         result = self.runner.invoke(main, ['--url', 'https://example.com'])
         # result.exception: 400: Error_Type - RequestException
@@ -61,32 +61,32 @@ class TestMain(unittest.TestCase):
     """
     FastAPI Test Client on API Router
     """
-    """ assert status code 405 with get /api """
     def test_fastapi_api_get_error(self):
+        """Assert status code 405 with get /api"""
         result = self.client.get("/api")
         self.assertEqual(result.status_code, 405)
 
-    """ assert api request with valid URL to result in success """
     def test_fastapi_api_post_success(self):
+        """Assert api request with valid URL to result in success"""
         result = self.client.post("/api", json={"url": "https://example.com"})
         self.assertEqual(result.status_code, 200)
 
-    """ assert status code 422 with post /api """
     def test_fastapi_api_post_error_nojson(self):
+        """Assert status code 422 with post /api"""
         result = self.client.post("/api")
         result_text = json.loads(result.text)
         self.assertEqual(result.status_code, 422)
         self.assertEqual(result_text.get("Error"), "Invalid data - Field required")
 
-    """ assert api request with empty URL to result in validation error """
     def test_fastapi_api_post_error_invalidjson(self):
+        """Assert api request with empty URL to result in validation error"""
         result = self.client.post("/api", json={"url": ""})
         result_text = json.loads(result.text)
         # result_text: {'Error': "Invalid data - (1) Expect data in JSON KV Pair Structure {'url': 'http(s)://xxxx'}"}
         self.assertIn("Expect data in JSON KV Pair Structure", str(result_text))
 
-    """ assert api request with empty JSON data to result in validation error """
     def test_fastapi_api_post_error_nulljson(self):
+        """Assert api request with empty JSON data to result in validation error"""
         result = self.client.post("/api", json={})
         result_text = json.loads(result.text)
         # result_text: {'Error': "Invalid data - (2) Expect data in JSON KV Pair Structure {'url': 'http(s)://xxxx'}"}
@@ -95,29 +95,29 @@ class TestMain(unittest.TestCase):
     """
     FastAPI Test Client on Health Router
     """
-    """ assert zero exit code with / to redirect to /health """
     def test_fastapi_health_success_redirect(self):
+        """Assert zero exit code with / to redirect to /health"""
         result = self.client.get("/")
         self.assertEqual(result.status_code, 200)
 
-    """ assert zero exit code with /health """
     def test_fastapi_health_success_direct(self):
+        """Assert zero exit code with /health"""
         result = self.client.get("/health")
         self.assertEqual(result.status_code, 200)
 
     """
     FastAPI Test Client on Versions Router
     """
-    """ assert zero exit code with /versions """
     def test_fastapi_url_versions(self):
+        """Assert zero exit code with /versions"""
         result = self.client.get("/versions")
         self.assertEqual(result.status_code, 200)
 
     """
     FastAPI Test Client on Info Router
     """
-    """ assert healthy status with /info """
     def test_fastapi_url_info_healthy(self):
+        """Assert healthy status with /info"""
         result = self.client.get("/info")
         result_text = json.loads(result.text)
         # result_text: {"status":"healthy","system":{"cpu_usage":"3.1%","mem_usage":"63.3%",
@@ -125,9 +125,9 @@ class TestMain(unittest.TestCase):
         self.assertEqual(result.status_code, 200)
         self.assertEqual(result_text.get("status"), "healthy")
 
-    """ assert warning status with /info """
     @patch('psutil.cpu_percent')
     def test_fastapi_url_info_warning(self, mock_cpu):
+        """Assert warning status with /info"""
         mock_cpu.return_value = 96.0
         result = self.client.get("/info")
         result_text = json.loads(result.text)
