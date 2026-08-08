4e20cedf48197e03a591408a32bfee44392d3ea2
Andre Choi <andreclc@gmail.com>
2026-08-07 18:49:27 -0700

    bump: version 0.2.1 → 0.3.0

--- src/pkg_15903/__init__.py
+++ src/pkg_15903/__init__.py
@@ -1,7 +1,7 @@
 from pkg_15903.core.cls_requests import ClsRequests
 from pkg_15903.utils.validation import ClsValidate
 
-__version__ = "0.2.1"
+__version__ = "0.3.0"
 
 
 __all__ = ["ClsRequests", "ClsValidate"]
